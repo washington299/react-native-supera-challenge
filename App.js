695c5baaa2ed4af4b00a011c8bac3f4b56bfff54
Washington <washingtoncampos9@gmail.com>
2020-12-01 02:02:56 -0300

    feat: add first react-navigation route

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
+
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
@@ -9,11 +13,21 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function App() {
+function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello World</Text>
-      <StatusBar style="dark" />
     </SafeAreaView>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+      <StatusBar style="dark" />
+    </NavigationContainer>
   );
 }
