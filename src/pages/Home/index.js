9db7609c7fb048a3c2252b9bb492e9a5718730cd
Washington <washingtoncampos9@gmail.com>
2020-12-01 14:44:55 -0300

    feat: create filter area

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Text } from 'react-native';
 
-const Home = ({ navigation }) => (
-  <View>
-    <Text>Hello World</Text>
-    <Button
-      title="Go to details"
-      onPress={() => {
-        navigation.navigate('Details', {
-          itemId: Math.floor(Math.random() * 100),
-          name: 'Washington Campos',
-        });
-      }}
-    />
-  </View>
+import Container, { FilterArea } from './styles';
+
+const Home = () => (
+  <Container>
+    <FilterArea>
+      <Text>Filter</Text>
+    </FilterArea>
+  </Container>
 );
 
 export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export default styled.View`
+  padding: 0 10px;
+`;
+
+export const FilterArea = styled.View`
+  height: 50px;
+  background-color: #FFE;
+`;
