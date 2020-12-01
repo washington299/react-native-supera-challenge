09cc104cc07b5cd9ae53e8e004706cf3935ba810
Washington <washingtoncampos9@gmail.com>
2020-12-01 01:25:10 -0300

    add eslint

--- App.js
+++ App.js
@@ -3,17 +3,17 @@ import { StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-      <StatusBar style="dark" />
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Hello World</Text>
+      <StatusBar style="dark" />
+    </SafeAreaView>
+  );
+}
