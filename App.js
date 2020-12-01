a61b8ad73cba0e4b54b65ac3b4d8ee9617fe3fd7
Washington <washingtoncampos9@gmail.com>
2020-12-01 04:20:33 -0300

    feat: import routes

--- App.js
+++ App.js
@@ -1,33 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 
-const Stack = createStackNavigator();
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-function Home() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello World</Text>
-    </SafeAreaView>
-  );
-}
+import Routes from './src/routes';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-      <StatusBar style="dark" />
+      <Routes />
+      <StatusBar style="light" />
     </NavigationContainer>
   );
 }
