c8f444ea457cb9aeeec0fa46520de420644c9287
Washington <washingtoncampos9@gmail.com>
2020-12-01 19:35:43 -0300

    feat: set up the url to request the data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import axios from 'axios';
 
-import Container, { FilterArea, ProductsArea, Product } from './styles';
+import Product from '../../components/ShoppingCartIcon/Product';
 
-const Home = () => (
-  <Container>
-    <FilterArea>
-      <Text>Filter</Text>
-    </FilterArea>
-    <ScrollView>
-      <ProductsArea>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-      </ProductsArea>
-    </ScrollView>
-  </Container>
-);
+import Container, { FilterArea, ProductsArea } from './styles';
+
+const Home = () => {
+  const [games, setGame] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const res = await axios.get('http://10.0.0.16:19001/products.json');
+      setGame(res.data);
+    }
+    getProducts();
+  }, []);
+
+  return (
+    <Container>
+      <FilterArea>
+        <Text>Filter</Text>
+      </FilterArea>
+      <ScrollView>
+        <ProductsArea>
+          {games.map((game) => (
+            <Product key={game.id} img={game.image} />
+          ))}
+        </ProductsArea>
+      </ScrollView>
+    </Container>
+  );
+};
 
 export default Home;
