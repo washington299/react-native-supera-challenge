ede37ed5b2cfb92ca98860eb72249b5dac774125
Washington <washingtoncampos9@gmail.com>
2020-12-02 18:18:24 -0300

    feat: create filters modal and toggle it when press the button

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Text } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/AntDesign';
 
 import api from '../../services/api';
 
 import Product from '../../components/Product';
 
-import Container, { FilterArea, ProductsArea } from './styles';
+import Container, {
+  ProductsArea,
+  FilterButton,
+  FilterArea,
+  FilterBox,
+} from './styles';
 
 const Home = () => {
   const [games, setGame] = useState([]);
+  const [hideFilters, setHideFilters] = useState(true);
 
   useEffect(() => {
     async function getProducts() {
@@ -23,11 +29,13 @@ const Home = () => {
   return (
     <Container>
       <ScrollView>
-        <FilterArea>
-          <Icon.Button name="filter" backgroundColor="#1e90ff" color="#FFF">
-            <Text style={{ fontSize: 18, color: '#FFF' }}>Filtros</Text>
-          </Icon.Button>
-        </FilterArea>
+        <TouchableOpacity onPress={() => setHideFilters(false)}>
+          <FilterButton>
+            <Icon.Button name="filter" backgroundColor="#1e90ff" color="#FFF">
+              <Text style={{ fontSize: 18, color: '#FFF' }}>Filtros</Text>
+            </Icon.Button>
+          </FilterButton>
+        </TouchableOpacity>
         <ProductsArea>
           {games.map((game) => (
             <Product
@@ -39,6 +47,21 @@ const Home = () => {
           ))}
         </ProductsArea>
       </ScrollView>
+      {!hideFilters && (
+        <FilterArea>
+          <FilterBox>
+            <Icon.Button
+              name="close"
+              backgroundColor="#FFF"
+              color="#AAA"
+              size={28}
+              style={{ alignSelf: 'flex-end' }}
+              onPress={() => setHideFilters(true)}
+            />
+            <Text>Area dos filtros</Text>
+          </FilterBox>
+        </FilterArea>
+      )}
     </Container>
   );
 };
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -5,7 +5,7 @@ export default styled.View`
   background-color: #FFF;
 `;
 
-export const FilterArea = styled.TouchableOpacity`
+export const FilterButton = styled.TouchableOpacity`
   width: 120px;
   justify-content: center;
   margin: 20px;
@@ -18,4 +18,25 @@ export const ProductsArea = styled.View`
   flex-direction: row;
   flex-wrap: wrap;
   padding: 20px 10px;
+`;
+
+export const FilterArea = styled.View`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  bottom: 0;
+  z-index: 1;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export const FilterBox = styled.View`
+  width: 100%;
+  height: 60%;
+  align-items: center;
+  justify-content: center;
+  background-color: #FFF;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  position: absolute;
+  bottom: 0;
 `;
