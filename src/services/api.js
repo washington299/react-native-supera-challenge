270b464c7f869d1653e838007d5c7e8d381bb043
Washington <washingtoncampos9@gmail.com>
2020-12-03 20:44:33 -0300

    feat: create filter functions

--- src/services/api.js
+++ src/services/api.js
@@ -10,6 +10,36 @@ const Api = {
     const res = await api(endpoint);
     return res.data;
   },
+  filterByPrice: async (endpoint) => {
+    const { data } = await api(endpoint);
+    const games = data;
+    const res = games.sort((a, b) => {
+      if (a.price < b.price) { return -1; }
+      if (a.price > b.price) { return 1; }
+      return 0;
+    });
+    return res;
+  },
+  filterByScore: async (endpoint) => {
+    const { data } = await api(endpoint);
+    const games = data;
+    const res = games.sort((a, b) => {
+      if (a.score < b.score) { return 1; }
+      if (a.score > b.score) { return -1; }
+      return 0;
+    });
+    return res;
+  },
+  filterByAlphaAsc: async (endpoint) => {
+    const { data } = await api(endpoint);
+    const games = data;
+    const res = games.sort((a, b) => {
+      if (a.name < b.name) { return -1; }
+      if (a.name > b.name) { return 1; }
+      return 0;
+    });
+    return res;
+  },
 };
 
 export default Api;
