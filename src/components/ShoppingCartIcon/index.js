567698925377e19e411ebed39a5cef540cba6860
Washington <washingtoncampos9@gmail.com>
2020-12-03 22:04:29 -0300

    feat: add shopping cart styles

--- src/components/ShoppingCartIcon/index.js
+++ src/components/ShoppingCartIcon/index.js
@@ -3,9 +3,14 @@ import { TouchableOpacity } from 'react-native';
 
 import CartIcon from '../../../assets/cart-icon.svg';
 
+import { CartQuantity, CartQuantityValue } from './styles';
+
 const ShoppingCartIcon = ({ navigation }) => (
   <TouchableOpacity onPress={() => navigation.navigate('Shopping')}>
     <CartIcon />
+    <CartQuantity>
+      <CartQuantityValue>0</CartQuantityValue>
+    </CartQuantity>
   </TouchableOpacity>
 );
 
---
+++ src/components/ShoppingCartIcon/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const CartQuantity = styled.View`
+  width: 20px;
+  height: 20px;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+  border-radius: 10px;
+  position: absolute;
+  right: 0;
+`;
+
+export const CartQuantityValue = styled.Text`
+  color: #FFF;
+  text-align: center;
+  font-size: 12px;
+  font-weight: bold;
+`;
