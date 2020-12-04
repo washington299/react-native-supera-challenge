0e738f6c5a2886b69fa383d32323bc66df4d83a6
Washington <washingtoncampos9@gmail.com>
2020-12-04 14:26:28 -0300

    feat: move products provider over the whole app

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 
+import ProductsProvider from './src/contexts/productsContext';
+
 import Routes from './src/routes';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Routes />
-      <StatusBar style="light" />
-    </NavigationContainer>
+    <ProductsProvider>
+      <NavigationContainer>
+        <Routes />
+        <StatusBar style="light" />
+      </NavigationContainer>
+    </ProductsProvider>
   );
 }
