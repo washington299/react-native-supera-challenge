008c3bfe6d569c8bb6ef5b0fb9fe146606bafe7a
Washington <washingtoncampos9@gmail.com>
2020-12-06 18:04:13 -0300

    ci: set up app to build on android

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.0.0.16:19001',
+  baseURL: 'https://exp.host/@washington299/react-native-supera-challenge',
   method: 'GET',
 });
 
