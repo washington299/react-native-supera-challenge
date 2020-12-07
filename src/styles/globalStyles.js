33244891becd4a8a3410a06d6272c843d1501bd5
Washington <washingtoncampos9@gmail.com>
2020-12-07 00:14:17 -0300

    feat: create global static images path

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,13 @@
+export default {
+  games: {
+    'call-of-duty-infinite-warfare.png': require('../../assets/call-of-duty-infinite-warfare.png'),
+    'call-of-duty-wwii.png': require('../../assets/call-of-duty-wwii.png'),
+    'fifa-18.png': require('../../assets/fifa-18.png'),
+    'horizon-zero-dawn.png': require('../../assets/horizon-zero-dawn.png'),
+    'mortal-kombat-xl.png': require('../../assets/mortal-kombat-xl.png'),
+    'shards-of-darkness.png': require('../../assets/shards-of-darkness.png'),
+    'super-mario-odyssey.png': require('../../assets/super-mario-odyssey.png'),
+    'terra-media-sombras-de-mordor.png': require('../../assets/terra-media-sombras-de-mordor.png'),
+    'the-witcher-iii-wild-hunt.png': require('../../assets/the-witcher-iii-wild-hunt.png'),
+  },
+};
